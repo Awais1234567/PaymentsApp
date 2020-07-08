3e9ade1ff9cc88ccc406110ac7980e72f35711bf
Muhammad Awais <awais.khalid@o3interfaces.com>
2020-07-08 15:25:40 +0500

    scheme shared

--- PaymentsApp/ViewController.swift
+++ PaymentsApp/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Welcome to Devops\nthis is the initial build to setup CI/CD"
+        label.textAlignment = .center
+        label.textColor = UIColor.darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor.red
+        view.backgroundColor = UIColor.white
+        setupViews()
+    }
+    func setupViews(){
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
     }
 }
 
