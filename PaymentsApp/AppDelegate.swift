609d7290c64b9c862b757ab2c5b6e1d1fdcc5d33
Muhammad Awais <awais.khalid@o3interfaces.com>
2020-07-08 14:24:43 +0500

    Initiale commit

---
+++ PaymentsApp/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  PaymentsApp
+//
+//  Created by Awais Khalid on 08/07/2020.
+//  Copyright © 2020 Awais Khalid. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var navController: UINavigationController!
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        presentController()
+        return true
+    }
+    func presentController(){
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let controller = ViewController()
+        navController = UINavigationController(rootViewController: controller)
+        navController.setNavigationBarHidden(true, animated: true)
+        window?.rootViewController = navController
+    }
+
+}
+
---
+++ PaymentsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaymentsApp
+//
+//  Created by Awais Khalid on 08/07/2020.
+//  Copyright © 2020 Awais Khalid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.red
+    }
+}
+
